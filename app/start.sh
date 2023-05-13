8850e958d9c277a104aefcb7415396c049b67e3d
gufin <gufinhaly@gmail.com>
2023-05-13 22:38:24 +0300

    corrections

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Wait for the database to start
+echo "Waiting for database to start..."
+while ! nc -z db 5432; do
+  sleep 1
+done
+
+echo "Database started"
+
 # Apply database migrations
 /app/bin/alembic upgrade head
 
